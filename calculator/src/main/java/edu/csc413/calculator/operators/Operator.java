472a3b70b2d44b6176b00dcacc32a05f846321ec
Gebriell <gt90903@gmail.com>
2024-01-11 19:33:54 -0800

    add: added divide operator

--- calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
@@ -30,8 +30,8 @@ public abstract class Operator {
         operators.put("+", new AddOperator());
         operators.put( "-", new SubtractOperator() );
         operators.put( "*", new MultiplyOperator() );
-        // operators.put( "-", new DivideOperator() );
-        // operators.put( "-", new PowerOperator() );
+        operators.put( "/", new DivideOperator() );
+        // operators.put( "**", new PowerOperator() );
     }
 
     /**
