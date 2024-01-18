7ad532c25539a21c9f7d3b59d5a8d901fc32d85a
Gebriell <gt90903@gmail.com>
2024-01-17 20:49:42 -0800

    feat: added operators to hashmap

--- calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
@@ -32,6 +32,9 @@ public abstract class Operator {
         operators.put( "*", new MultiplyOperator() );
         operators.put( "/", new DivideOperator() );
         operators.put( "^", new PowerOperator() );
+        operators.put( "(", new LeftParenthesis());
+        operators.put( ")", new RightParenthesis());
+
     }
 
     /**
